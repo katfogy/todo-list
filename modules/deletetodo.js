d6c35f556297796b732f54ee8353077fb929e245
katfogy <katfogy@gmail.com>
2023-05-03 12:00:39 +0100

    delete function added

---
+++ modules/deletetodo.js
@@ -0,0 +1,18 @@
+import TodoList from './todoclass.js';
+
+const alltodos = document.getElementById('to-do-list');
+
+const deletetodo = () => {
+  alltodos.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-trash')) {
+      const { id } = e.target;
+      let todos = JSON.parse(localStorage.getItem('todos')) || [];
+      todos = todos.filter((todo) => todo.index !== id);
+      localStorage.setItem('todos', JSON.stringify(todos));
+      const todo = new TodoList();
+      todo.todosList();
+    }
+  });
+};
+
+export default deletetodo;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './style.css';
 import addTodo from '../modules/addtodo.js';
 import TodoList from '../modules/todoclass.js';
+import deletetodo from '../modules/deletetodo.js';
 
 const todoclass = new TodoList();
 todoclass.todosList();
 addTodo();
+
+deletetodo();
