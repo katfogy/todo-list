b592e4ec4ecb1560626537c440d41098fc237133
katfogy <katfogy@gmail.com>
2023-05-04 16:40:53 +0100

    complete check done

--- modules/interactivetodo.js
+++ modules/interactivetodo.js
@@ -1,31 +1,21 @@
-import TodoList from './todoclass.js';
+const interacttodo = () => {
+  const checklist = document.querySelectorAll('.mycheck');
+  for (let i = 0; i < checklist.length; i += 1) {
+    checklist[i].addEventListener('change', (e) => {
+      const { id } = e.target;
+      const todos = JSON.parse(localStorage.getItem('todos')) || [];
+      const search = todos.find((item) => item.index.toString() === id);
+      if (e.target.checked) {
+        if (search !== undefined) {
+          search.completed = !search.completed;
+          search.index = id;
+        }
+        document.location.reload();
+      }
 
-
-const interacttodo=()=>{
-    let checklist=document.querySelectorAll('.mycheck');
-   for(let i=0; i<checklist.length;i+=1){
-    checklist[i].addEventListener('change',(e)=>{
-        let id=e.target.id
-        let todos = JSON.parse(localStorage.getItem('todos')) || [];
-        let search=todos.find(item=>item.index.toString() ===id)
-        if(e.target.checked){
-           if(search!==undefined){
-            search.completed=!search.completed;
-            search.index=id
-           }
-               document.location.reload();
-            }
-            
-            localStorage.setItem('todos', JSON.stringify(todos));
-          
-    //         const refresh = new TodoList();
-    // refresh.todosList();
-    // document.location.reload();
-            
-    })
-   
-   }
- 
-}
+      localStorage.setItem('todos', JSON.stringify(todos));
+    });
+  }
+};
 
 export default interacttodo;
